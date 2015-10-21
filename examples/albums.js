590ac0debabd06cb03fec7fbe30d51bedff1ef73
Jonathan Leonard <johanatan@gmail.com>
2015-10-21 01:55:33 -0700

    Fix typo.

--- examples/albums.js
+++ examples/albums.js
@@ -4,7 +4,7 @@ var gql = require('graphql');
 var albums = [
   {'id': 1, 'name': 'Dark Side Of The Moon', 'releaseDate': 'March 1, 1973', 'artist': 'Pink Floyd'},
   {'id': 2, 'name': 'The Beatles', 'releaseDate': 'November 22, 1968', 'artist': 'The Beatles'},
-  {'id': 3, 'name': 'The Wall', 'releaseDate': 'Auguest 1, 1982', 'artist': 'Pink Floyd'}];
+  {'id': 3, 'name': 'The Wall', 'releaseDate': 'August 1, 1982', 'artist': 'Pink Floyd'}];
 var dataResolver = {"query":  function (typename, predicate) {
   console.assert(typename == "Album");
   if (predicate == "all()") return albums;
